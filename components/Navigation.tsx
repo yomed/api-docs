7c2a7409a00b5b9f9f0d86041bbb55668c7eda2d
Addison <42930383+addisonschultz@users.noreply.github.com>
2020-06-30 16:58:34 +0200

    Add navigation docs under Beta flag (#111)
    * Add navigation docs
    
    * Add useIsInCurrentNavigationTarget to Utilities (#113)
    
    * Update library to 1.2.4
    
    * Add useIsInCurrentTarget to utilities
    
    Co-authored-by: Adam Seckel <adam.seckel@gmail.com>

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -70,6 +70,7 @@ const LibraryMenu = () => {
             <MenuItem className="data" href="/pages/data.mdx" title="Data &amp; Overrides" />
             <MenuItem className="canvas-components" href="/pages/canvas-components.mdx" title="CanvasComponents" />
             <MenuItem className="property-controls" href="/pages/property-controls.mdx" title="PropertyControls" />
+            <MenuItem className="navigation" href="/pages/navigation.mdx" title="Navigation" />
             <MenuItem className="render-target" href="/pages/render-target.mdx" title="RenderTarget" />
         </Menu>
     )
